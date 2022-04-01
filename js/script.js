64c554f932ac4148a3269f8e53620fd63f28c8d6
babayaga110 <karan.19jics046@jietjodhpur.ac.in>
2022-04-01 18:02:16 +0530

    nav complete

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+function mouseover(){
+    document.querySelector(".dropdown").style.display = "block";
+}
+function mouseout(){
+    document.querySelector(".dropdown").style.display = "none";
+}
